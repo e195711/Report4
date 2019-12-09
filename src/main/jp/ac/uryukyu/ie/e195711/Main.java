f53ce9273d7b8a7b12f2bea62aa69340756e4754
Ko-ta Shingaki <e195711@ie.u-ryukyu.ac.jp>
2019-12-09 14:22:47 +0900

    parseIntメソッド実行

--- src/main/jp/ac/uryukyu/ie/e195711/Main.java
+++ src/main/jp/ac/uryukyu/ie/e195711/Main.java
@@ -1,7 +1,9 @@
 package jp.ac.uryukyu.ie.e195711;
+import java.lang.Integer;
 
 public class Main {
     public static void main(String[] args) {
+        /*
         try {
             String str = null;
             System.out.println(str.length());
@@ -9,5 +11,9 @@ public class Main {
             System.out.println("NullPointerExceptionエラーが発生しました。");
             System.out.println(e.getMessage());
         }
+
+         */
+        String str = "百二十三";
+        int value = Integer.parseInt(str);
     }
 }
